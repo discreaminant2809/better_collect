bd5fc85519957f030f5fe9064ce556e44a8902a1
discreaminant2809 <phaomaxe2015@gmail.com>
2025-12-01 12:13:36 -0800

    Added doc for aggregate_struct macro. Not testable yet.

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -14,6 +14,82 @@ pub use imp::*;
 pub use into_aggregate::*;
 pub use ref_aggregate_op::*;
 
+#[macro_export]
+// Somehow the doc.rs does not render the feature flag.
+#[cfg_attr(docsrs, doc(cfg(feature = "unstable")))]
+/// Combines multiple aggregate ops into a single "`struct`-based" aggregate.
+///
+/// The `struct` can be constructed normally (even with a base struct),
+/// except every field is "initialized"
+/// with an [`AggregateOp`] whose [`Value`] matches that field's type.
+///
+/// Among the specified fields to be aggregated, every field except the last one
+/// **must implement [`RefAggregateOp`]**.
+///
+/// # Limitations
+///
+/// This macro currently supports structs with **1 to 12 aggregated fields**
+/// (i.e., fields explicitly specified with an aggregate op, not those
+/// filled via a base struct such as `..Default::default()`).
+///
+/// # Examples
+///
+/// ```
+/// use better_collect::{aggregate_struct, aggregate::{self, GroupMap}};
+///
+/// #[derive(Default)]
+/// struct Stats {
+///     sum: i32,
+///     max: i32,
+///     version: u32,
+/// }
+///
+/// let groups = [(1, 1), (1, 4), (2, 1), (1, 2), (2, 3)]
+///     .into_iter()
+///     .better_collect(
+///         HashMap::new()
+///             .into_aggregate(aggregate_struct!(Stats {
+///                 sum: aggregate::Sum::new().copying(),
+///                 max: aggregate::Max::new(),
+///                 ..Default::default()
+///             }))
+///     );
+///
+/// let expected_groups = HashMap::from_iter([
+///     (1, Stats { sum: 7, max: 4, version: 0 }),
+///     (2, Stats { sum: 4, max: 3, version: 0 }),
+/// ]);
+/// assert_eq!(groups, expected_groups);
+/// ```
+///
+/// [`Value`]: AggregateOp::Value
+macro_rules! aggregate_struct {
+    (
+        $ty_name:ident {
+            $($fields:ident: $aggregate_ops:expr,)+
+            $(..$base_struct:expr)?
+        }
+    ) => {
+        $crate::aggregate::Combine::new(
+            ($($aggregate_ops,)+),
+            |_, ($($fields,)+)| $ty_name { $($fields,)+ $(..$base_struct)? },
+            |$ty_name { $($fields,)+ }| ($($fields,)+),
+        )
+    };
+
+    (
+        $ty_name:ident {
+            $($fields:ident: $aggregate_ops:expr),+
+        }
+    ) => {
+        $crate::aggregate_struct!(
+            $ty_name {
+                $($fields: $aggregate_ops,)+
+            }
+        )
+    };
+}
+
 #[inline(always)]
 const fn assert_op<Op: AggregateOp>(op: Op) -> Op {
     op
