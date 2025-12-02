f834cb5bfb62084980ec58bcff720df10c177501
discreaminant2809 <phaomaxe2015@gmail.com>
2025-12-02 12:13:05 -0800

    Fixed the vec module.

--- src/imp/vec.rs
+++ src/imp/vec.rs
@@ -1,8 +1,9 @@
-//! A [`Collector`] for [`Vec`].
+//! [`Collector`]s for [`Vec`].
 //!
 //! This module corresponds to [`mod@std::vec`].
 //!
 //! [`Collector`]: crate::Collector
+
 use crate::RefCollector;
 
 use std::ops::ControlFlow;
@@ -21,38 +22,16 @@ use alloc::vec::Vec;
 /// [`Output`]: crate::Collector::Output
 pub struct IntoCollector<T>(Vec<T>);
 
-/// A [`Collector`] that pushes collected items into a [`Vec`].
+/// A [`Collector`] that pushes collected items into a [`&mut Vec`](Vec).
 /// Its [`Output`] is [`&mut Vec`](Vec).
 ///
 /// This also implements [`RefCollector`] if `T` is [`Copy`].
 ///
-/// This struct is created by [`Vec::collector()`](VecExt::collector).
+/// This struct is created by `Vec::collector_mut()`.
 ///
 /// [`Collector`]: crate::Collector
 /// [`Output`]: crate::Collector::Output
-pub struct Collector<'a, T>(&'a mut Vec<T>);
-
-/// Extends methods for [`Vec`].
-#[allow(private_bounds)]
-pub trait VecExt<T>: Sealed {
-    /// Creates a [`Collector`] that pushes collected items into a [`Vec`].
-    /// The `Vec` is mutably borrowed.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use better_collect::{Collector, vec::VecExt};
-    ///
-    /// let mut v = vec![1, 1];
-    /// let mut collector = v.collector();
-    /// collector.collect_many([1; 3]);
-    ///
-    /// assert_eq!(v, [1; 5]);
-    /// ```
-    ///
-    /// [`Collector`]: crate::Collector
-    fn collector(&mut self) -> Collector<'_, T>;
-}
+pub struct CollectorMut<'a, T>(&'a mut Vec<T>);
 
 impl<T> crate::IntoCollector for Vec<T> {
     type Item = T;
@@ -72,11 +51,11 @@ impl<'a, T> crate::IntoCollector for &'a mut Vec<T> {
 
     type Output = Self;
 
-    type IntoCollector = Collector<'a, T>;
+    type IntoCollector = CollectorMut<'a, T>;
 
     #[inline]
     fn into_collector(self) -> Self::IntoCollector {
-        Collector(self)
+        CollectorMut(self)
     }
 }
 
@@ -116,7 +95,7 @@ impl<T: Copy> RefCollector for IntoCollector<T> {
     }
 }
 
-impl<'a, T> crate::Collector for Collector<'a, T> {
+impl<'a, T> crate::Collector for CollectorMut<'a, T> {
     type Item = T;
     type Output = &'a mut Vec<T>;
 
@@ -144,21 +123,10 @@ impl<'a, T> crate::Collector for Collector<'a, T> {
     }
 }
 
-impl<T: Copy> RefCollector for Collector<'_, T> {
+impl<T: Copy> RefCollector for CollectorMut<'_, T> {
     #[inline]
     fn collect_ref(&mut self, item: &mut T) -> ControlFlow<()> {
         self.0.push(*item);
         ControlFlow::Continue(())
     }
 }
-
-impl<T> VecExt<T> for Vec<T> {
-    #[inline]
-    fn collector(&mut self) -> Collector<'_, T> {
-        Collector(self)
-    }
-}
-
-trait Sealed {}
-
-impl<T> Sealed for Vec<T> {}
