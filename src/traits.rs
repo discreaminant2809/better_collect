2df24dcee1e69c107684269a3adc65f4bb179126
discreaminant2809 <phaomaxe2015@gmail.com>
2025-12-01 18:17:17 -0800

    Added CollectorByRef and CollectorByMut.

--- src/traits.rs
+++ src/traits.rs
@@ -1,9 +1,13 @@
 mod better_collect;
 mod collector;
+mod collector_by_mut;
+mod collector_by_ref;
 mod into_collector;
 mod ref_collector;
 
 pub use better_collect::*;
 pub use collector::*;
+pub use collector_by_mut::*;
+pub use collector_by_ref::*;
 pub use into_collector::*;
 pub use ref_collector::*;
---
+++ src/traits/collector_by_ref.rs
@@ -0,0 +1,42 @@
+use crate::{Collector, IntoCollector};
+
+/// A type that can be converted into a collector by **shared reference**.
+///
+/// This trait's main purpose is to provide a convenience method to creates
+/// a collector from `&T`.
+///
+/// You cannot implement this trait directly.
+/// Instead, you should implement [`IntoCollector`] for `&T` (where `T` is your type)
+/// and this trait is automatically implemented for `T`.
+///
+/// This trait is not intended for use in bounds.
+/// Use [`IntoCollector`] in trait bounds instead.
+#[allow(private_bounds)]
+pub trait CollectorByRef: Sealed {
+    /// Which collector being produced?
+    type Collector<'a>: Collector
+    where
+        Self: 'a;
+
+    /// Creates a [`Collector`] from a shared reference of a value.
+    fn collector(&self) -> Self::Collector<'_>;
+}
+
+impl<T> CollectorByRef for T
+where
+    for<'a> &'a T: IntoCollector,
+{
+    type Collector<'a>
+        = <&'a T as IntoCollector>::IntoCollector
+    where
+        T: 'a;
+
+    #[inline]
+    fn collector(&self) -> Self::Collector<'_> {
+        self.into_collector()
+    }
+}
+
+trait Sealed {}
+
+impl<T> Sealed for T where for<'a> &'a T: IntoCollector {}
