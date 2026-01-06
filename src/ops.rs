3dc542b643f4a98941f53a5ca1e3737dbcafb5d8
discreaminant2809 <phaomaxe2015@gmail.com>
2026-01-05 17:49:24 -0800

    Added doc for the crate::ops module.

--- src/ops.rs
+++ src/ops.rs
@@ -1,4 +1,8 @@
+//! [`Collector`]s that repeatedly apply common operators in [`std::ops`].
 //!
+//! This module corresponds to [`std::ops`].
+//!
+//! [`Collector`]: crate::collector::Collector
 
 mod product;
 mod sum;
