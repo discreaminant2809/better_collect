b562beb3202b27296f8906178985c4646b788139
discreaminant2809 <phaomaxe2015@gmail.com>
2026-01-11 19:02:26 -0800

    Added tests for crate::iter::Fold(Ref).

--- src/iter/fold.rs
+++ src/iter/fold.rs
@@ -212,3 +212,96 @@ impl<A: Debug, T, F> Debug for FoldRef<A, T, F> {
             .finish()
     }
 }
+
+#[cfg(all(test, feature = "std"))]
+mod proptests {
+    use proptest::collection::vec as propvec;
+    use proptest::prelude::*;
+    use proptest::test_runner::TestCaseResult;
+
+    use crate::test_utils::{BasicCollectorTester, CollectorTesterExt, PredError};
+
+    use super::*;
+
+    proptest! {
+        /// Here, we will use the Kadane's Algorithm to test fold.
+        /// [`Fold`](super::Fold)
+        #[test]
+        fn all_collect_methods(
+            nums in propvec(any::<i32>(), ..=9),
+        ) {
+            all_collect_methods_impl(nums)?;
+        }
+
+        /// [`FoldRef`](super::FoldRef)
+        #[test]
+        fn all_collect_methods_ref(
+            nums in propvec(any::<i32>(), ..=5),
+        ) {
+            all_collect_methods_ref_impl(nums)?;
+        }
+    }
+
+    fn all_collect_methods_impl(nums: Vec<i32>) -> TestCaseResult {
+        BasicCollectorTester {
+            iter_factory: || nums.iter().copied(),
+            collector_factory: || {
+                Fold::new(KADANE_INIT, |(sum, max_sum), num| {
+                    kadane_fold(sum, max_sum, num)
+                })
+            },
+            should_break_pred: |_| false,
+            pred: |iter, output, remaining| {
+                let expected = iter.fold(KADANE_INIT, |(mut sum, mut max_sum), num| {
+                    kadane_fold(&mut sum, &mut max_sum, num);
+                    (sum, max_sum)
+                });
+
+                // We also check the `sum`
+                if expected != output {
+                    Err(PredError::IncorrectOutput)
+                } else if remaining.next().is_some() {
+                    Err(PredError::IncorrectIterConsumption)
+                } else {
+                    Ok(())
+                }
+            },
+        }
+        .test_collector()
+    }
+
+    fn all_collect_methods_ref_impl(nums: Vec<i32>) -> TestCaseResult {
+        BasicCollectorTester {
+            iter_factory: || nums.iter().copied(),
+            collector_factory: || {
+                Fold::new_ref(KADANE_INIT, |(sum, max_sum), &mut num| {
+                    kadane_fold(sum, max_sum, num)
+                })
+            },
+            should_break_pred: |_| false,
+            pred: |iter, output, remaining| {
+                let expected = iter.fold(KADANE_INIT, |(mut sum, mut max_sum), num| {
+                    kadane_fold(&mut sum, &mut max_sum, num);
+                    (sum, max_sum)
+                });
+
+                if expected != output {
+                    Err(PredError::IncorrectOutput)
+                } else if remaining.next().is_some() {
+                    Err(PredError::IncorrectIterConsumption)
+                } else {
+                    Ok(())
+                }
+            },
+        }
+        .test_ref_collector()
+    }
+
+    fn kadane_fold(sum: &mut i32, max_sum: &mut Option<i32>, num: i32) {
+        *sum = num;
+        *max_sum = (*max_sum).max(Some(*sum));
+        *sum = (*sum).max(0);
+    }
+
+    const KADANE_INIT: (i32, Option<i32>) = (0, None);
+}
