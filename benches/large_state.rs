46fad2c3e252d021e6598b56c6f8debe3ffcea4c
discreaminant2809 <phaomaxe2015@gmail.com>
2026-02-13 14:01:22 -0800

    Added large_state benchmark.

---
+++ benches/large_state.rs
@@ -0,0 +1,68 @@
+use std::{hint::black_box, time::Duration};
+
+use better_collect::{iter::Fold, prelude::*};
+use criterion::{Criterion, criterion_group, criterion_main};
+use rand::{RngExt, SeedableRng, rngs::StdRng};
+
+fn large_state(criterion: &mut Criterion) {
+    let seed = 0;
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    let nums: Box<_> = std::iter::repeat_with(|| rng.random_range(0..1_000))
+        .take(500_000)
+        .collect();
+
+    println!("Seed: {seed}");
+    println!("First 10 elements: {:?}", &nums[..10]);
+
+    let mut group = criterion.benchmark_group("large_state");
+
+    macro_rules! bench_fn {
+        ($fn_name:ident) => {
+            group.bench_function(stringify!($fn_name), |bencher| {
+                bencher.iter(|| $fn_name(black_box(&nums)));
+            });
+        };
+    }
+
+    unsafe {
+        bench_fn!(fold);
+        bench_fn!(bc);
+        bench_fn!(for_loop);
+    }
+
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default()
+        .warm_up_time(Duration::from_secs(5))
+        .measurement_time(Duration::from_secs(30))
+        .sample_size(300);
+    targets = large_state
+}
+criterion_main!(benches);
+
+unsafe fn fold(nums: &[usize]) -> [usize; 1000] {
+    nums.iter().fold([0; 1000], |mut counts, &num| {
+        unsafe { *counts.get_unchecked_mut(num) += 1 };
+        counts
+    })
+}
+
+unsafe fn bc(nums: &[usize]) -> [usize; 1000] {
+    nums.iter().feed_into(Fold::new([0; 1000], |counts, &num| {
+        unsafe { *counts.get_unchecked_mut(num) += 1 };
+    }))
+}
+
+unsafe fn for_loop(nums: &[usize]) -> [usize; 1000] {
+    let mut counts = [0; 1000];
+
+    for &num in nums {
+        unsafe { *counts.get_unchecked_mut(num) += 1 };
+    }
+
+    counts
+}
--- benches/sum_max.rs
+++ benches/sum_max.rs
@@ -17,15 +17,21 @@ fn sum_max(criterion: &mut Criterion) {
 
     let mut group = criterion.benchmark_group("sum_max");
 
-    group.bench_function("with_initial", |bencher| {
-        // bencher.iter(|| black_box(fold_w_initial(&nums)));
-        bencher.iter(|| black_box(bc_tee_with_fold(&nums)));
-    });
+    macro_rules! bench_fn {
+        ($fn_name:ident) => {
+            group.bench_function(stringify!($fn_name), |bencher| {
+                bencher.iter(|| $fn_name(black_box(&nums)));
+            });
+        };
+    }
 
-    group.bench_function("without_initial", |bencher| {
-        // bencher.iter(|| black_box(fold_wo_initial(&nums)));
-        bencher.iter(|| black_box(bc_tee_with_max(&nums)));
-    });
+    bench_fn!(two_pass);
+    bench_fn!(fold_w_initial);
+    bench_fn!(fold_wo_initial);
+    bench_fn!(bc_tee_with_fold);
+    bench_fn!(bc_tee_with_max);
+    bench_fn!(for_loop_w_initial);
+    bench_fn!(for_loop_wo_initial);
 
     group.finish();
 }
@@ -34,36 +40,60 @@ criterion_group! {
     name = benches;
     config = Criterion::default()
         .warm_up_time(Duration::from_secs(5))
-        .measurement_time(Duration::from_secs(15))
+        .measurement_time(Duration::from_secs(30))
         .sample_size(300);
     targets = sum_max
 }
 criterion_main!(benches);
 
-#[allow(unused)]
 fn fold_w_initial(nums: &[i32]) -> (i32, i32) {
     nums.iter()
         .copied()
         .fold((0, i32::MIN), |(sum, max), num| (sum + num, max.max(num)))
 }
 
-#[allow(unused)]
 fn fold_wo_initial(nums: &[i32]) -> (i32, Option<i32>) {
     nums.iter()
         .copied()
         .fold((0, None), |(sum, max), num| (sum + num, max.max(Some(num))))
 }
 
-#[allow(unused)]
+fn bc_tee_with_fold(nums: &[i32]) -> (i32, i32) {
+    nums.iter()
+        .copied()
+        .feed_into(i32::adding().tee(Fold::new(i32::MIN, |max, num| *max = (*max).max(num))))
+}
+
 fn bc_tee_with_max(nums: &[i32]) -> (i32, Option<i32>) {
     nums.iter()
         .copied()
         .feed_into(i32::adding().tee(Max::new()))
 }
 
-#[allow(unused)]
-fn bc_tee_with_fold(nums: &[i32]) -> (i32, i32) {
-    nums.iter()
-        .copied()
-        .feed_into(i32::adding().tee(Fold::new(i32::MIN, |max, num| *max = (*max).max(num))))
+fn two_pass(nums: &[i32]) -> (i32, Option<i32>) {
+    (nums.iter().sum(), nums.iter().copied().max())
+}
+
+fn for_loop_w_initial(nums: &[i32]) -> (i32, i32) {
+    let mut sum = 0;
+    let mut max = i32::MIN;
+
+    for &num in nums {
+        sum += num;
+        max = max.max(num);
+    }
+
+    (sum, max)
+}
+
+fn for_loop_wo_initial(nums: &[i32]) -> (i32, Option<i32>) {
+    let mut sum = 0;
+    let mut max = None;
+
+    for &num in nums {
+        sum += num;
+        max = max.max(Some(num));
+    }
+
+    (sum, max)
 }
